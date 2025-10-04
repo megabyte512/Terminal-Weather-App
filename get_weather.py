eedce89878b1559efed889be048cf93939ec6c03
Tyler <tyler@legion.tail5bfd45.ts.net>
2025-10-04 12:01:09 -0600

    Progress on app: - Modified get_weather.py to output packaged weather data for weather.sh - Finished most of the backend that needs to happen in weather.sh 	* unpacked the output from get_weather.py 	* created a loading symbol function 	* started on the tput smcup for the frontend display

--- get_weather.py
+++ get_weather.py
@@ -7,6 +7,19 @@ headers = {
 
 loc_url = "https://ipinfo.io/json"
 nws_url = "https://api.weather.gov/points/"
+
+# cleaning the "Short Forecast" from NWS
+unwanted_forecast_jargon = ['Scattered', 'And', 'then', 'Slight', 'Chance', 'Partly', 'Mostly', ' ', 'mph', 'Likely', 'Showers']
+def strip(s):
+    for removable in unwanted_forecast_jargon:
+        if s.find(removable) != -1:
+            s = s.replace(removable, '')
+    if s.find('Rain') > -1:
+        s = s.replace('Sunny', '')
+        s = s.replace('Cloudy', '')
+    return s
+
+
 response = requests.get(loc_url)
 
 if response.status_code == 200: 
@@ -18,8 +31,7 @@ if response.status_code == 200:
     location = data['loc']
     zip = data['postal']
 else:
-    print("locfail")
-
+    print('locfail')
 
 response = requests.get(f"{nws_url}{location}", headers=headers)
 
@@ -29,10 +41,9 @@ if response.status_code == 200:
     forecast_url = properties['forecast']
     hourly_url = properties['forecastHourly']
     grid_data_url = properties['forecastGridData']
-    #print(f"forecast url:{forecast_url} hourly url:{hourly_url} grid data url:{grid_data_url}") # for viewing json structure
+    # print(f"forecast url:{forecast_url} hourly url:{hourly_url} grid data url:{grid_data_url}") # for viewing json structure
 else:
     print('nwsfail')
-
 # failure check for requests not necessary now that we already know we can connect to NWS
 
 # TODO: print data as cleanly as possible for weather.sh
@@ -42,12 +53,11 @@ hourly_response = requests.get(hourly_url, headers=headers)
 current_weather = hourly_response.json()['properties']['periods'][0]
 print(current_weather['temperature'])
 print(current_weather['probabilityOfPrecipitation']['value'])
-print(current_weather['dewpoint']['value'])
+print(round(current_weather['dewpoint']['value'], 2))
 print(current_weather['relativeHumidity']['value'])
-print(current_weather['windSpeed'])
+print(strip(current_weather['windSpeed']))
 print(current_weather['windDirection'])
-print(current_weather['shortForecast'])
-print()
+print(strip(current_weather['shortForecast']))
 
 # Weekly weather output
 weekly_response = requests.get(forecast_url, headers=headers)
@@ -55,7 +65,8 @@ weekly_weather = weekly_response.json()['properties']['periods']
 for i in range(14):
     print(weekly_weather[i]['temperature'])
     print(weekly_weather[i]['probabilityOfPrecipitation']['value'])
+    print(strip(weekly_weather[i]['shortForecast']))
 
-# For more detailed outputs if I decide to add them
+# For more detailed outputs if I decide to add them (like a radar map)
 #response = requests.get(grid_data_url, headers=headers)
 #print(response.json())
--- weather.sh
+++ weather.sh
@@ -1,3 +1,88 @@
-# TODO: Refresh button, tabs for (tbd, maybe simple current forecast with cloud coverage, temp, sunrise/sunset, wind, humidity; weekly forecast; and a detailed forecast with aqi, uv index dew point, pressure, visibility, moon phase, maybe even a radar idk, etc.; whatever else I think of), ascii art, live clock, and whatever else I can think of
+#!/bin/bash
 
-weather_data=$(python3 /home/tyler/.local/bin/Terminal-Weather-App/get_weather.py)
+# TODO: tabs for (tbd, maybe simple current forecast with cloud coverage, temp, sunrise/sunset, wind, humidity; weekly forecast; and a detailed forecast with aqi, uv index dew point, pressure, visibility, moon phase, maybe even a radar idk, etc.; whatever else I think of), ascii art, live clock, and whatever else I can think of
+
+
+load_symbol() {
+    local spin='|/-\'
+    local i=0
+    
+    while ps -p $1 > /dev/null 2>&1; do
+        i=$(( (i+1) % 4 ))
+        printf "\rConnecting to NWS [${spin:$i:1}] "
+        sleep 0.1
+    done
+    printf "\r\033[K"
+}
+
+# Start Python in background, redirect output to temp file
+python3 /home/tyler/.local/bin/Terminal-Weather-App/get_weather.py > /tmp/weather_$$.txt &
+python_pid=$!
+
+# Show load_symbol
+load_symbol $python_pid
+
+# Wait for completion and get result
+wait $python_pid
+raw_weather_data=$(cat /tmp/weather_$$.txt)
+rm /tmp/weather_$$.txt
+
+
+
+if [[ $raw_weather_data = "locfail" ]]; then
+  echo "Failed to retrieve location"
+elif [[ $raw_weather_data = "nwsfail" ]]; then
+  echo "Failed to connect to NWS"
+else
+  IFS=$'\n' read -r -d '' -a weather_data_array <<< "$raw_weather_data"   
+fi
+
+#for item in "${weather_data_array[@]}"; do
+#  echo "$item"
+#done
+
+# unpacking python output:
+current_temperature="${weather_data_array[0]}°F"
+current_rain_probability="${weather_data_array[1]}%"
+current_dewpoint="${weather_data_array[2]}°F"
+current_realtive_humidity="${weather_data_array[3]}%"
+current_windspeed="${weather_data_array[4]}mph"
+current_wind_direction="${weather_data_array[5]}"
+current_forecast="${weather_data_array[6]}"
+
+day0=("${weather_data_array[@]:7:3}")
+night0=("${weather_data_array[@]:10:3}") 
+day1=("${weather_data_array[@]:13:3}")
+night1=("${weather_data_array[@]:16:3}")
+day2=("${weather_data_array[@]:19:3}")
+night2=("${weather_data_array[@]:22:3}")
+day3=("${weather_data_array[@]:25:3}")
+night3=("${weather_data_array[@]:28:3}")
+day4=("${weather_data_array[@]:31:3}")
+night4=("${weather_data_array[@]:34:3}")
+day5=("${weather_data_array[@]:37:3}")
+night5=("${weather_data_array[@]:40:3}")
+day6=("${weather_data_array[@]:43:3}")
+night6=("${weather_data_array[@]:46:3}")
+
+tput smcup
+tput civis
+
+trap 'tput cnorm; tput rmcup' INT TERM EXIT
+
+#tabs() {}
+
+#current_tab() {}
+#weekly_tab() {}
+# under_construction_tab() {}
+
+while true; do
+  read -n 1 -s key
+
+  case $key in
+    1) echo "1" ;;
+    2) echo "2" ;;
+    3) echo "3" ;;
+    q) exit 0 ;;
+  esac
+done
